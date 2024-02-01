373d016ec4eb83e5b2c5300663b847c4c8800936
Taegyu <59700057+TaegyuHan@users.noreply.github.com>
2024-02-01 21:50:05 +0900

    Add: #2 MySQL 연동

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.demo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("/")
     public String index() {
         return "Hellow world!";
+    }
+
+    @GetMapping("/check-mysql-connection")
+    public String checkMysqlConnection() {
+        try {
+            jdbcTemplate.execute("SELECT 1");
+            return "MySQL 연결 확인: 성공";
+        } catch (Exception ex) {
+            return "MySQL 연결 확인: 실패 - " + ex.getMessage();
+        }
     }
 }
